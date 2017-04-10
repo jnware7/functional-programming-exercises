7f161296f337d81a996af4616c2b4cf836fc7f4e
jnware7 <jnware7@gmail.com>
2017-04-10 16:38:42 -0700

    part1-exercises

--- mag-part-1/curry/curry_exercises.js
+++ mag-part-1/curry/curry_exercises.js
@@ -6,17 +6,21 @@ var _ = require('ramda');
 //==============
 // Refactor to remove all arguments by partially applying the function
 
+// var words = function(str) {
+//   return split(' ', str);
+// };
 var words = function(str) {
-  return split(' ', str);
+  return str.split(' ');
 };
-
 // Exercise 1a
 //==============
 // Use map to make a new words fn that works on an array of strings.
 
-var sentences = undefined;
+// var sentences = undefined;
 
-
+var sentences = function(array){
+  return array.map(words)
+}
 // Exercise 2
 //==============
 // Refactor to remove all arguments by partially applying the functions
@@ -25,34 +29,52 @@ var filterQs = function(xs) {
   return filter(function(x){ return match(/q/i, x);  }, xs);
 };
 
-
+var filterQs = function(xs) {
+  return xs.filter(match(/q/i));
+};
 // Exercise 3
 //==============
 // Use the helper function _keepHighest to refactor max to not reference any arguments
 
 // LEAVE BE:
 var _keepHighest = function(x,y){ return x >= y ? x : y; };
-
+//
+// REFACTOR THIS ONE:
+// var max = function(xs) {
+//   return reduce(function(acc, x){
+//     return _keepHighest(acc, x);
+//   }, -Infinity, xs);
+// };
+//
 // REFACTOR THIS ONE:
 var max = function(xs) {
-  return reduce(function(acc, x){
-    return _keepHighest(acc, x);
-  }, -Infinity, xs);
+  return xs.reduce( _keepHighest, -Infinity);
 };
 
-  
+
 // Bonus 1:
 // ============
 // wrap array's slice to be functional and curried.
 // //[1,2,3].slice(0, 2)
-var slice = undefined;
-
+// var slice = undefined;
+var slice = function(num1){
+  return function(num2){
+    return function(array) {
+      return array.slice(num1, num2)
+    }
+  }
+}
 
 // Bonus 2:
 // ============
 // use slice to define a function "take" that takes n elements. Make it curried
-var take = undefined;
+// var take = undefined;
 
+var take = function(num){
+  return function(array){
+      return array.slice(0,num)
+  }
+}
 
 module.exports = { words: words,
                    sentences: sentences,
