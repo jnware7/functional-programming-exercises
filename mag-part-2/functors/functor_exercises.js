46612fe1545c8315850600e6a70a96b0053e5235
jnware7 <jnware7@gmail.com>
2017-04-12 13:09:59 -0700

    functorExercises

--- mag-part-2/functors/functor_exercises.js
+++ mag-part-2/functors/functor_exercises.js
@@ -5,8 +5,8 @@ var _ = require('ramda');
 // Exercise 1
 // ==========
 // Use _.add(x,y) and _.map(f,x) to make a function that increments a value inside a functor
-
-var ex1 = undefined;
+var add =  function(x){return _.add(x,1)}
+var ex1 = function(y){return _.map(add,y)}
 
 
 
@@ -15,18 +15,18 @@ var ex1 = undefined;
 // Use _.head to get the first element of the list
 var xs = Identity.of(['do', 'ray', 'me', 'fa', 'so', 'la', 'ti', 'do']);
 
-var ex2 = undefined;
+var ex2 = function(xs){return _.map(_.head, xs)}
 
 
 
 // Exercise 3
 // ==========
 // Use safeProp and _.head to find the first initial of the user
-var safeProp = _.curry(function (x, o) { return Maybe.of(o[x]); });
-
+// var safeProp = _.curry(function (x, o) { return Maybe.of(o[x]); });
+ var safeProp = _.curry(function (x) { return Maybe.of(x); });
 var user = { id: 2, name: "Albert" };
 
-var ex3 = undefined;
+var ex3 = function(user){return safeProp(_.head(user.name))}
 
 
 
@@ -34,28 +34,30 @@ var ex3 = undefined;
 // ==========
 // Use Maybe to rewrite ex4 without an if statement
 
-var ex4 = function (n) {
-  if (n) { return parseInt(n); }
-};
+// var ex4 = function (n) {
+//   if (n) { return parseInt(n); }
+// };
 
-var ex4 = undefined;
+var ex4 = _.curry(function (n){ return Maybe.of(parseInt(n))})
 
 
 
 // Exercise 5
 // ==========
 // Write a function that will getPost then _.toUpper the post's title
-
+//
 // getPost :: Int -> Future({id: Int, title: String})
 var getPost = function (i) {
   return new Task(function(rej, res) {
     setTimeout(function(){
-      res({id: i, title: 'Love them futures'})  
+      res({id: i, title: 'Love them futures'})
     }, 300)
   });
 };
 
-var ex5 = undefined;
+var ex5 = undefined
+//function(num){return getPost(num)}
+// .then (_.compose(_.map(_toUpper, _.map(_.prop('title'))))}
 
 
 
@@ -66,10 +68,11 @@ var ex5 = undefined;
 var showWelcome = _.compose(_.concat( "Welcome "), _.prop('name'));
 
 var checkActive = function(user) {
- return user.active ? Right.of(user) : Left.of('Your account is not active')
+ return user.active ? Right.of(showWelcome(user)) : Left.of('Your account is not active')
 };
 
-var ex6 = undefined;
+var ex6 = user=>{return checkActive(user)}
+
 
 
 
@@ -78,7 +81,7 @@ var ex6 = undefined;
 // Write a validation function that checks for a length > 3. It should return Right(x) if it is greater than 3 and Left("You need > 3") otherwise
 
 var ex7 = function(x) {
-  return undefined; // <--- write me. (don't be pointfree)
+  return x.length > 3 ? Right.of(x) : Left.of('You need > 3'); // <--- write me. (don't be pointfree)
 };
 
 
@@ -94,6 +97,8 @@ var save = function(x) {
   });
 };
 
-var ex8 = undefined;
+var eithers = function(str){return str.length > 3 ? save(user) : ex7(user)}
+var ex8 = function(eithers){return either(eithers)}
+
 
 module.exports = {ex1: ex1, ex2: ex2, ex3: ex3, ex4: ex4, ex5: ex5, ex6: ex6, ex7: ex7, ex8: ex8};
